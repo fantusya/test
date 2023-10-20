0d8890df21fbd73d68e70c7fe7b5fbb62f2a4043
Anastasiia <guba.anastaysha@gmail.com>
2023-10-21 02:08:40 +0300

    deleted footer

--- src/components/Footer/Footer.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterEl = styled.footer`
-  padding: 18px 0px;
-
-  background-color: ${p => p.theme.colors.header};
-`;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import Hero from 'components/Hero';
 import DealsSection from 'components/DealsSection';
-import Footer from 'components/Footer';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <DealsSection />
-      <Footer />
     </>
   );
 };
