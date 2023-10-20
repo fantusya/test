e686dfbf6f5e4632eaf7acc835d05ff9e0e6cd46
Anastasiia <guba.anastaysha@gmail.com>
2023-10-21 02:08:21 +0300

    added render depending on state

--- src/components/DealsSection/DealsSection.jsx
+++ src/components/DealsSection/DealsSection.jsx
@@ -5,70 +5,32 @@ import { Container } from 'globalStyles/globalStyles';
 import { fetchDeals } from 'api/deals';
 
 import CardsList from 'components/CardsList';
-
 import { DealsTitle } from './DealsSection.styled';
 
-// const array = [
-//   {
-//     id: 1,
-//     title: 'The Marina Torch',
-//     price: 6500000,
-//     ticket: 60000,
-//     yield: 9.25,
-//     daysLeft: 150,
-//     sold: 75,
-//     currency: 'Dhs',
-//     img: 'http://res.cloudinary.com/dpjmbj5vx/image/upload/v1697478557/barmenAvatars/652d560af0a1a8525c7e1c9a.jpg',
-//   },
-//   {
-//     id: 2,
-//     title: 'The Marina Torch',
-//     price: 6500000,
-//     ticket: 60000,
-//     yield: 9.25,
-//     daysLeft: 150,
-//     sold: 75,
-//     currency: 'Dhs',
-//     img: 'http://res.cloudinary.com/dpjmbj5vx/image/upload/v1697478557/barmenAvatars/652d560af0a1a8525c7e1c9a.jpg',
-//   },
-//   {
-//     id: 3,
-//     title: 'The Marina Torch',
-//     price: 6500000,
-//     ticket: 60000,
-//     yield: 9.25,
-//     daysLeft: 150,
-//     sold: 75,
-//     currency: 'Dhs',
-//     img: 'http://res.cloudinary.com/dpjmbj5vx/image/upload/v1697478557/barmenAvatars/652d560af0a1a8525c7e1c9a.jpg',
-//   },
-//   {
-//     id: 4,
-//     title: 'The Marina Torch',
-//     price: 6500000,
-//     ticket: 60000,
-//     yield: 9.25,
-//     daysLeft: 150,
-//     sold: 75,
-//     currency: 'Dhs',
-//     img: 'http://res.cloudinary.com/dpjmbj5vx/image/upload/v1697478557/barmenAvatars/652d560af0a1a8525c7e1c9a.jpg',
-//   },
-// ];
+import Refreshing from 'components/Refreshing';
+import NotFound from 'components/NotFound';
+
+const Status = {
+  pending: 'pending',
+  resolved: 'resolved',
+  rejected: 'rejected',
+  idle: 'idle',
+};
 
 const DealsSection = () => {
   const [deals, setDeals] = useState([]);
+  const [status, setStatus] = useState('');
 
   useEffect(() => {
     async function getDeals() {
-      // setStatus(Status.PENDING);
+      setStatus(Status.pending);
 
       try {
         const fetchedDeals = await fetchDeals();
-        console.log('DEALS');
         setDeals(fetchedDeals);
-        // setStatus(Status.RESOLVED);
+        setStatus(Status.resolved);
       } catch (error) {
-        // setStatus(Status.REJECTED);
+        setStatus(Status.rejected);
         console.log(error);
       }
     }
@@ -80,7 +42,9 @@ const DealsSection = () => {
       <Container>
         <DealsTitle id="deals">Open Deals</DealsTitle>
         <Box>
-          <CardsList array={deals} />
+          {status === Status.resolved && <CardsList array={deals} />}
+          {status === Status.pending && <Refreshing />}
+          {status === Status.rejected && <NotFound />}
         </Box>
       </Container>
     </Box>
